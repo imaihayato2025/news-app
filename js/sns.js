e15190625e113ba094ab50a305b1771446dab5b7
imaihayato2025 <imaihayato2024@gmail.com>
2025-06-25 09:14:57 +0900

    最新

--- js/sns.js
+++ js/sns.js
@@ -1,13 +1,8 @@
-const snsBtn = document.querySelector('.sns_btn');
-const btn01 = document.querySelector('.snsbtn01');
-const btn02 = document.querySelector('.snsbtn02');
-const btn03 = document.querySelector('.snsbtn03');
-const btn04 = document.querySelector('.snsbtn04');
+const shareBtn = document.querySelector('.share');
+const snsBtns = document.querySelectorAll('.all_sns'); // Allのまま
 
-snsBtn.addEventListener('click', () => {
-  if (btn01.style.display === 'block') {
-    btn01.style.display = 'none';
-  } else {
-    btn01.style.display = 'block';
-  }
+shareBtn.addEventListener('click', () => {
+  snsBtns.forEach(btn => {
+    btn.classList.toggle('active');
+  });
 });
