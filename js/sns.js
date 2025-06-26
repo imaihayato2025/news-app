888589d42f02430359c2395d0610a48f5dbb3d33
imahaya <imaihayato2024@gmail.com>
2025-06-26 09:17:24 +0900

    Delete js/sns.js

--- js/sns.js
+++
@@ -1,8 +0,0 @@
-const shareBtn = document.querySelector('.share');
-const snsBtns = document.querySelectorAll('.all_sns'); // Allのまま
-
-shareBtn.addEventListener('click', () => {
-  snsBtns.forEach(btn => {
-    btn.classList.toggle('active');
-  });
-});
